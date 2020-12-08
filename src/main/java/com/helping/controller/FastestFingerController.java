dde05718188a024e6383e25195731673a2d6a603
vishalrajapure <vishalrajapure@gmail.com>
2020-12-08 20:29:30 +0530

    Updating code

--- src/main/java/com/helping/controller/FastestFingerController.java
+++ src/main/java/com/helping/controller/FastestFingerController.java
@@ -1,18 +1,28 @@
 package com.helping.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/fastestFinger")
 public class FastestFingerController {
 
+    static List<Integer> range = null;
+
 
     @GetMapping("/register")
     public void registerCandidate() {
-
+        range = IntStream.range(0, 100).boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+        Collections.shuffle(range);
+        range.subList(0, 99).forEach(System.out::println);
     }
 
 }
